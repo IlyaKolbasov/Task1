49456d522f226e5d308f7b97133adecd0ce229be
IlyaKolbasov <kolbiskolbis@gmail.com>
2025-02-18 19:51:19 +0300

    Init

---
+++ src/main/java/kolbasov/task1/HelloServlet.java
@@ -0,0 +1,25 @@
+package kolbasov.task1;
+
+import java.io.*;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        message = "Hello Word!";
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+
+}
