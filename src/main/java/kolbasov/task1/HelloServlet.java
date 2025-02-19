c4463407c889aeaa72a127f1cb5e525dcd89975b
IlyaKolbasov <kolbiskolbis@gmail.com>
2025-02-19 21:53:15 +0300

    finish

--- src/main/java/kolbasov/task1/HelloServlet.java
+++ src/main/java/kolbasov/task1/HelloServlet.java
@@ -13,7 +13,7 @@ public class HelloServlet extends HttpServlet {
 
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        message = "Hello people!";
+        message = "Hello PEOOOOOPl!";
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
