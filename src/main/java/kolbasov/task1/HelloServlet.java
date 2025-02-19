e2d7a40fbaf782b233e560427b2ebc82bc6bbf1f
IlyaKolbasov <kolbiskolbis@gmail.com>
2025-02-19 22:06:34 +0300

    jenkinsfile

--- src/main/java/kolbasov/task1/HelloServlet.java
+++ src/main/java/kolbasov/task1/HelloServlet.java
@@ -13,7 +13,7 @@ public class HelloServlet extends HttpServlet {
 
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        message = "Hello PEOOOOOPl!";
+        message = "Hello PEOOOOOPlSSSS1S!";
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
